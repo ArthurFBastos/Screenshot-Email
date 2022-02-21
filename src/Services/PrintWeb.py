32b87b3bec3e9f21dbfbadd616a19fb1a1f2991a
Arthur Bastos <afbastos8@gmail.com>
2022-02-20 22:17:28 -0300

    :tada: style: Traduzir para inglês e comentar cada linha

--- src/Services/PrintWeb.py
+++ src/Services/PrintWeb.py
@@ -1,27 +1,35 @@
+
 from turtle import screensize
 import pygetwindow
 import pyautogui
 from PIL import Image
 import time
-n = 1
-path = f"C:\\Users\\Acer\\Desktop\\Arquivos\\Py\\Screenshot_Email\\src\\Services\\Prints\\Print{n}.png"
 
-titles = pygetwindow.getAllTitles()
+def print_web(path):
 
-time.sleep(5)
-window = pygetwindow.getWindowsWithTitle('Google Chrome')[0]
-#print(window)
-x1 = window.left
-y1 = window.top
-height = window.height
-width = window.width
+    #If you need the Titleof a window run these commands 
+    #titles = pygetwindow.getAllTitles()
+    #print(titles)
 
-x2 = x1 + width
-y2 = y1 + height
+    #the code sleeps for 5 seconds (Allow you to minimize any window open)
+    time.sleep(5)
 
-pyautogui.screenshot(path)
+    #Select the window according to the Title and get the size of it
+    window = pygetwindow.getWindowsWithTitle('Google Chrome')[0]
+    #print(window)
+    x1 = window.left
+    y1 = window.top
+    height = window.height
+    width = window.width
 
-im = Image.open(path)
-im =im.crop((x1,y1,x2,y2))
-im.save(path)
-im.show(path)
+    x2 = x1 + width
+    y2 = y1 + height
+    
+    #Take the screenshot according to the window size 
+    #If there is any window open in front of the selected the image overlaps the other,so leave just the selected window open
+    pyautogui.screenshot(path)
+    
+    im = Image.open(path)
+    im =im.crop((x1,y1,x2,y2))
+    im.save(path)
+    im.show(path)

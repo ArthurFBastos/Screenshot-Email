343326a4682b7b730a5ed4a9bfd3b85c2f81e5fd
Arthur Bastos <afbastos8@gmail.com>
2022-02-20 22:22:36 -0300

    :guitar: feat: Adicionar função para pegar data atual

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
-from Services.SendEmail import send_email, test
+from Services.PrintWeb import print_web
+from Services.SendEmail import send_email
+from datetime import datetime
+from Services.SendEmail import send_email2
+#Get atual date
+date = datetime.today().strftime('%m-%d-%y')    
+path = f"C:\\Users\\Acer\\Desktop\\Arquivos\\Py\\Screenshot_Email\\Prints\\Print  {date}.png"
 
+print_web(path)
 
-send_email()
+send_email2()

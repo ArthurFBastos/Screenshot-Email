f4e39a225d605bbdb61e01b7dea7112b101159ab
Arthur Bastos <afbastos8@gmail.com>
2022-02-20 22:20:36 -0300

    :recycle: refactor: Adicionar SendGrid como API para o envio de Email

--- src/Services/SendEmail.py
+++ src/Services/SendEmail.py
@@ -5,55 +5,84 @@ from email.mime.text import MIMEText
 
 from email.mime.base import MIMEBase
 from email import encoders
+from Services.PrintWeb import print_web
 
-from config import database_infos
+import os
+from dotenv import load_dotenv
 
-def send_email():
-    #Setar o Servidor SMTP
-    host = "smtp.gmail.com"
-    port = "587"
-    login = "emailenvio@email.com"
-    password = "password"
-    destino = "emaildestino@email.com"
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
-    #Dando start no servidor 
+load_dotenv()
+
+
+def send_email2():
+    message = Mail(
+        from_email=os.getenv("login"),
+        to_emails=os.getenv("send_to"),
+        subject='Sending with Twilio SendGrid is Fun',
+        html_content="""
+        <b> Title </b>
+        <p> First text line </p>
+        """
+    )
+
+    sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+
+
+
+def send_email(path):
+
+    #Set SMTP server and login in your email
+    host = os.getenv("host")
+    port = os.getenv("port")
+    login = os.getenv("login")
+    password = os.getenv("senha")
+    send_to = os.getenv("send_to")
+
+    #Starting the server
     server = smtplib.SMTP(host,port)
     server.ehlo()
     server.starttls()
     server.login(login,password)
 
 
-    #Construir o email tipo MIME
-    corpo = """
-    <b> Primeira linha </b>
-    <p> Segunda linha de texto</p>
+    #Write MIME type email in html
+    body = """
+    <b> Title </b>
+    <p> First text line </p>
     """
 
 
-    #Montando e-mail 
+    #Build email body
     email_msg = MIMEMultipart()
     email_msg['From'] = login
-    email_msg['To'] = destino
-    email_msg['Subject'] = "Assunto do email"
-    email_msg.attach(MIMEText(corpo,'html'))
+    email_msg['To'] = send_to
+    email_msg['Subject'] = "Write a subject"
+    email_msg.attach(MIMEText(body,'html'))
 
-    #Abrimos o arquivo em modo leitura e binary 
-    cam_arquivo = "Caminho do arquivo com separação de 2 barras \\"
-    attchment = open(cam_arquivo,'rb')
+    #Open the archive in reading mode, must be binary
+    path_arquive = f"{path}"
+    attchment = open(path_arquive,'rb')
 
-    #Lemos o arquivo no modo binario e jogamos codificado em base 64 (que é o que o e-mail precisa )
+    #We read the file in binary mode and play it encoded in base 64 (which is what email needs)
     att = MIMEBase('application', 'octet-stream')
     att.set_payload(attchment.read())
     encoders.encode_base64(att)
 
-    #Adicionado o cabeçalho no tipo anexo de email 
-    att.add_header('Content-Disposition', f'attachment; filename=Nome_do_arquivo.tipo')
-    #Fechamos o arquivo 
+    #Added header in email attachment type
+    att.add_header('Content-Disposition', f'attachment; filename=Archive_name.jpg')
+    
+    #Close the archive
     attchment.close()
 
-    #Colocamos o anexo no corpo do e-mail 
+    #Add the attachment to the body
     email_msg.attach(att)
 
-    #Enviar o email tipo MIME no servidor SMTP 
+    #Send MIME type email on SMTP server
     server.sendmail(email_msg['From'],email_msg['To'],email_msg.as_string())
     server.quit()
